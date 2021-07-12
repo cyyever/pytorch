938a64f0f7f094417ef56a53742446522d57dc7f
cyy <cyyever@outlook.com>
2021-07-13 01:37:01 +0800

    add script

---
+++ rebase.sh
@@ -0,0 +1,12 @@
+# for i in $(seq 50)
+# do
+#   git reset HEAD~
+# done
+# # git fetch --all
+# # git rebase up/master
+git rebase origin/use_dynamic_cast
+git rebase origin/fix_pointers
+git rebase origin/build_system_fix
+git rebase origin/miscellaneous_fixes
+git rebase origin/zero_init
+git rebase origin/use_default_constructor
