2f63e2e3f1a30e997224e82621bbaffbbb8abad1
cyy <cyyever@outlook.com>
2023-01-05 12:26:46 +0800

    fix python detection logic

--- tools/build_pytorch_libs.py
+++ tools/build_pytorch_libs.py
@@ -81,9 +81,7 @@ def build_caffe2(
 ) -> None:
     my_env = _create_build_env()
     build_test = not check_negative_env_flag("BUILD_TEST")
-    cmake.generate(
-        version, cmake_python_library, build_python, build_test, my_env, rerun_cmake
-    )
+    cmake.generate(version, build_python, build_test, my_env, rerun_cmake)
     if cmake_only:
         return
     cmake.build(my_env)
