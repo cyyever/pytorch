ec34c1aa451e6828a6e62ee06f4fcf170afcf717
cyy <cyyever@outlook.com>
2024-08-06 13:30:36 +0800

    Fix clang-tidy warnings

--- aten/src/ATen/native/cpu/SumKernel.cpp
+++ aten/src/ATen/native/cpu/SumKernel.cpp
@@ -18,7 +18,7 @@ template <typename acc_t, typename scalar_t, typename F>
 Vectorized<acc_t> load_reduce_vec(const scalar_t* data, F reduce, acc_t ident) {
   using vec_t = Vectorized<scalar_t>;
   using vacc_t = Vectorized<acc_t>;
-  static_assert(vacc_t::size() <= vec_t::size(), "");
+  static_assert(vacc_t::size() <= vec_t::size());
   const auto val = vec_t::loadu(data);
   alignas(64) std::array<scalar_t, vec_t::size()> values;
   val.store(values.data());
@@ -142,7 +142,7 @@ struct OuterSumCastLoadPolicy <vec_t, vacc_t,
   }
 
   static vacc_t load(const char * C10_RESTRICT data, int64_t stride, int64_t index) {
-    static_assert(vacc_t::size() <= vec_t::size(), "");
+    static_assert(vacc_t::size() <= vec_t::size());
     const auto val = vec_t::loadu(data + stride * index, vacc_t::size());
     alignas(64) scalar_t values[vec_t::size()];
     val.store(values);
@@ -303,7 +303,7 @@ template <typename StorePolicy, typename scalar_t>
 static void store(char * C10_RESTRICT data, int64_t stride, int64_t index,
                   const Vectorized<scalar_t> &values) {
   using vec_t = Vectorized<scalar_t>;
-  alignas(64) std::array<scalar_t, vec_t::size()> array_values;
+  alignas(64) std::array<scalar_t, vec_t::size()> array_values{};
   values.store(array_values.data());
   store<StorePolicy>(data, stride, index, array_values);
 }
@@ -354,9 +354,7 @@ std::array<scalar_t, nrows> multi_row_sum(
   const int64_t level_step = (1 << level_power);
   const int64_t level_mask = level_step - 1;
 
-  // NOLINTNEXTLINE(modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
-  scalar_t acc[num_levels][nrows];
-  std::fill_n(&acc[0][0], num_levels * nrows, scalar_t(0));
+  std::array<std::array<scalar_t, nrows>, num_levels> acc{};
 
   int64_t i = 0;
   for (; i + level_step <= size;) {
@@ -405,12 +403,7 @@ std::array<scalar_t, nrows> multi_row_sum(
     }
   }
 
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
-  std::array<scalar_t, nrows> ret;
-  for (const auto k : c10::irange(nrows)) {
-    ret[k] = acc[0][k];
-  }
-  return ret;
+  return acc[0];
 }
 
 template <typename scalar_t, typename LoadPolicy>
