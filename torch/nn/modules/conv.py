5f808b0125cf21c6fd57cb387a3ddc6a282ebae5
Yuanyuan Chen <cyyever@outlook.com>
2025-09-26 13:44:04 +0800

    remove unneeded mypy suppression
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -1476,9 +1476,7 @@ class LazyConv1d(_LazyConvXdMixin, Conv1d):  # type: ignore[misc]
     .. seealso:: :class:`torch.nn.Conv1d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = Conv1d  # type: ignore[assignment]
+    cls_to_become = Conv1d
 
     def __init__(
         self,
@@ -1545,9 +1543,7 @@ class LazyConv2d(_LazyConvXdMixin, Conv2d):  # type: ignore[misc]
     .. seealso:: :class:`torch.nn.Conv2d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = Conv2d  # type: ignore[assignment]
+    cls_to_become = Conv2d
 
     def __init__(
         self,
@@ -1615,9 +1611,7 @@ class LazyConv3d(_LazyConvXdMixin, Conv3d):  # type: ignore[misc]
     .. seealso:: :class:`torch.nn.Conv3d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = Conv3d  # type: ignore[assignment]
+    cls_to_become = Conv3d
 
     def __init__(
         self,
@@ -1682,9 +1676,7 @@ class LazyConvTranspose1d(_LazyConvXdMixin, ConvTranspose1d):  # type: ignore[mi
     .. seealso:: :class:`torch.nn.ConvTranspose1d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = ConvTranspose1d  # type: ignore[assignment]
+    cls_to_become = ConvTranspose1d
 
     def __init__(
         self,
@@ -1751,9 +1743,7 @@ class LazyConvTranspose2d(_LazyConvXdMixin, ConvTranspose2d):  # type: ignore[mi
     .. seealso:: :class:`torch.nn.ConvTranspose2d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = ConvTranspose2d  # type: ignore[assignment]
+    cls_to_become = ConvTranspose2d
 
     def __init__(
         self,
@@ -1820,9 +1810,7 @@ class LazyConvTranspose3d(_LazyConvXdMixin, ConvTranspose3d):  # type: ignore[mi
     .. seealso:: :class:`torch.nn.ConvTranspose3d` and :class:`torch.nn.modules.lazy.LazyModuleMixin`
     """
 
-    # super class define this variable as None. "type: ignore[..] is required
-    # since we are redefining the variable.
-    cls_to_become = ConvTranspose3d  # type: ignore[assignment]
+    cls_to_become = ConvTranspose3d
 
     def __init__(
         self,
