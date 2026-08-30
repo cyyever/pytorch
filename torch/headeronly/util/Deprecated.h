245423d7ed191520c5294cfb9bd18938bb7b993e
cyy <cyyever@outlook.com>
2026-08-30 16:59:04 +0800

    Collapse Deprecated.h to the arm C++26 always takes
    C10_DEPRECATED and C10_DEPRECATED_MESSAGE were selected by a three-arm ladder
    testing __cplusplus >= 201402L, then __GNUC__, then #warning. At C++26 the first
    arm always wins on both compilers this fork builds with, so the other two were
    unreachable.
    
    The comment above the ladder claimed MSVC uses a __declspec(deprecated)
    implementation, but no _MSC_VER arm exists in this tree: MSVC has no working
    __cplusplus without /Zc:__cplusplus, so it would have landed on the #warning arm
    that leaves C10_DEPRECATED_MESSAGE undefined. That path was already broken
    before this change; removing it does not make MSVC less supported than it was.
    
    The C10_DEFINE_DEPRECATED_USING chain below is untouched. Its __CUDACC__
    distinction is live: the __has_cpp_attribute block excludes nvcc, so nvcc really
    does fall through to the arm underneath.
    
    Test Plan: the same ten translation units as the previous commit, including the
    two compiled by nvcc, which is the compiler the surviving branch exists for.
    0 errors.

--- torch/headeronly/util/Deprecated.h
+++ torch/headeronly/util/Deprecated.h
@@ -15,22 +15,8 @@
 //      ...
 //    };
 
-// NB: __cplusplus doesn't work for MSVC, so for now MSVC always uses
-// the "__declspec(deprecated)" implementation and not the C++14
-// "[[deprecated]]" attribute. We tried enabling "[[deprecated]]" for C++14 on
-// MSVC, but ran into issues with some older MSVC versions.
-#if (defined(__cplusplus) && __cplusplus >= 201402L)
 #define C10_DEPRECATED [[deprecated]]
 #define C10_DEPRECATED_MESSAGE(message) [[deprecated(message)]]
-#elif defined(__GNUC__)
-#define C10_DEPRECATED __attribute__((deprecated))
-// TODO Is there some way to implement this?
-#define C10_DEPRECATED_MESSAGE(message) __attribute__((deprecated))
-
-#else
-#warning "You need to implement C10_DEPRECATED for this compiler"
-#define C10_DEPRECATED
-#endif
 
 // Sample usage:
 //
@@ -40,32 +26,12 @@
 //
 //    using BadType [[deprecated]] = int;
 
-// technically [[deprecated]] syntax is from c++14 standard, but it works in
-// many compilers.
-#if defined(__has_cpp_attribute)
-#if __has_cpp_attribute(deprecated) && !defined(__CUDACC__)
-#define C10_DEFINE_DEPRECATED_USING(TypeName, TypeThingy) \
-  using TypeName [[deprecated]] = TypeThingy;
-#endif
-#endif
-
-
-#if !defined(C10_DEFINE_DEPRECATED_USING) && defined(__GNUC__)
-// nvcc has a bug where it doesn't understand __attribute__((deprecated))
-// declarations even when the host compiler supports it. We'll only use this gcc
-// attribute when not cuda, and when using a GCC compiler that doesn't support
-// the c++14 syntax we checked for above (available in __GNUC__ >= 5)
-#if !defined(__CUDACC__)
-#define C10_DEFINE_DEPRECATED_USING(TypeName, TypeThingy) \
-  using TypeName __attribute__((deprecated)) = TypeThingy;
-#else
-// using cuda + gcc < 5, neither deprecated syntax is available so turning off.
+// nvcc does not understand a deprecation attribute on a using-declaration, so
+// the alias is defined without one there.
+#if defined(__CUDACC__)
 #define C10_DEFINE_DEPRECATED_USING(TypeName, TypeThingy) \
   using TypeName = TypeThingy;
-#endif
-#endif
-
-#if !defined(C10_DEFINE_DEPRECATED_USING)
-#warning "You need to implement C10_DEFINE_DEPRECATED_USING for this compiler"
-#define C10_DEFINE_DEPRECATED_USING
+#else
+#define C10_DEFINE_DEPRECATED_USING(TypeName, TypeThingy) \
+  using TypeName [[deprecated]] = TypeThingy;
 #endif
